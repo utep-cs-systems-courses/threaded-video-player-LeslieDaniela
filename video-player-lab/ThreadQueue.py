239f8adbe3e3eb84fb305fef7e7071d0ecfb8195
LeslieDaniela <33047756+LeslieDaniela@users.noreply.github.com>
2020-12-03 12:33:17 -0700

    Update ThreadQueue.py

--- video-player-lab/ThreadQueue.py
+++ video-player-lab/ThreadQueue.py
@@ -4,8 +4,8 @@ class ThreadQueue(): # queue implemented with python list
     def __init__(self):
         self.queue = []
         self.lock = threading.Lock()
-        self.full = threading.Semaphore(0)
-        self.empty = threading.Semaphore(24)
+        self.full = threading.Semaphore(10)
+        self.empty = threading.Semaphore(0)
 
     def put(self, item): # enqueue
         self.empty.acquire()
